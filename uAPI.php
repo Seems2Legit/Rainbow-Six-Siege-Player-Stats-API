ddcc7a10bea7b250c66dfc0e371f9487c30a6c1c
Seems2Legit <Seems2Legit@users.noreply.github.com>
2018-05-27 12:31:48 +0200

    Update uAPI.php

--- uAPI.php
+++ uAPI.php
@@ -4,7 +4,7 @@
  * @copyright 2017
  * @version 2.0.1.2
  * github.com/K4CZP3R
- * Updated at 27-May-2017 by Seems2Legit (getStats,login,searchUser)
+ * Updated at 27-May-2018 by Seems2Legit (getStats,login,searchUser)
  */
 class ubiapi{
 	private $b64authcreds;
