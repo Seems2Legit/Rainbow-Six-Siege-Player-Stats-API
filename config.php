04f3e6d85987b1a3f2e62d35bbd752209fa7ec09
Seems2Legit <Seems2Legit@users.noreply.github.com>
2018-05-28 17:45:12 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array(
+
+	"appcode" => "",
+	"ubi-email" => "",
+	"ubi-password" => "",
+	"default-region" => "emea", //Regions: emea - ncsa - apac 
+	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills" 
+	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
+	
+);
+
+
+?>
