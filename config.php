ee3b370f2547cd840eb532d3842ba7c2cd7eadea
Seems2Legit <Seems2Legit@users.noreply.github.com>
2018-06-21 15:44:46 +0200

    Added Platform argument to config

--- config.php
+++ config.php
@@ -6,9 +6,9 @@ $config = array(
 	"ubi-email" => "",
 	"ubi-password" => "",
 	"default-region" => "emea", //Regions: emea - ncsa - apac 
-	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills" 
+	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", 
 	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
-	
+	"default-platform" => "uplay" //Platforms: uplay / xbl / psn
 );
 
 
