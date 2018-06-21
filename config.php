3c5f8321762467f5018cc6af78c236e5e876647b
Seems2Legit <Seems2Legit@users.noreply.github.com>
2018-06-21 15:48:23 +0200

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $config = array(
 	"default-region" => "emea", //Regions: emea - ncsa - apac 
 	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", 
 	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
-	"default-platform" => "uplay" //Platforms: uplay / xbl / psn
+	"default-platform" => "uplay" //Platforms: uplay - xbl - psn
 );
 
 
