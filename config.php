65920cca0aff9588d79fdcc59411f42a54124f35
Seems2Legit <Seems2Legit@users.noreply.github.com>
2018-06-22 16:42:00 +0200

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $config = array(
 	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", 
 	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
 	"default-platform" => "uplay", //Platforms: uplay - xbl - psn
-	"default-progression" => true //Enabled: true - false
+	"default-progression" => "true" //Enabled: true - false
 );
 
 
