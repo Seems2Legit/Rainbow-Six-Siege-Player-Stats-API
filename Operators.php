ce59a3d584e786e36b97764aeae1b4794488d672
sidelux <redfenix45@gmail.com>
2020-11-23 16:19:07 +0100

    Fixed Operators missing bug

--- Operators.php
+++ Operators.php
@@ -7,11 +7,7 @@ $arrContextOptions = array(
 	),
 );
 
-$ubi_page = file_get_contents("https://game-rainbow6.ubi.com/it-it/home", false, stream_context_create($arrContextOptions));
-preg_match("/main(.+)js/", $ubi_page, $matches);
-$mainjs = file_get_contents("https://game-rainbow6.ubi.com/assets/scripts/main" . $matches[1] . "js", false, stream_context_create($arrContextOptions));
-preg_match("/assets\/data\/operators(.+?)json/", $mainjs, $matches);
-$operators_string = file_get_contents("https://game-rainbow6.ubi.com/assets/data/operators" . $matches[1] . "json", false, stream_context_create($arrContextOptions));
+$operators_string = file_get_contents("https://pastebin.com/raw/hA1tnH4G", false, stream_context_create($arrContextOptions));
 
 $json = json_decode($operators_string, true);
 $arr_result = new \stdClass;
